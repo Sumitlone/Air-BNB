8b737c824b78f7f7949dcd34a1d3bab16c0ee3ab
Sumit Lone <sumitlone1912@gmail.com>
2025-10-10 01:30:38 +0530

    passport package installed and created demo user

--- app.js
+++ app.js
@@ -7,17 +7,12 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listing = require("./routes/listing.js");
 const review = require("./routes/review.js");
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
-app.engine("ejs", ejsMate);
 
 // Database connection
 main()
@@ -33,12 +28,13 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
-let port = 8080;
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.engine("ejs", ejsMate);
 
 const sessionOptions = {
   secret:"secretkey",
@@ -59,18 +55,35 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
+app.get("/demouser", async (req, res) => {
+  let newUser = new User({
+    email: "demouser@gmail.com",
+    username: "demoUser",
+  });
+
+  let regUser = await User.register(newUser, "password");
+  res.send(regUser);
+});
+
+
 app.use("/listing", listing);
 app.use("/listing/:id/review",review);
 
 
-
-app.all("*splat", (req, res, next) => {
+app.all("{*splat}", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
 
@@ -78,6 +91,12 @@ app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
   res.status(statusCode).render("error.ejs", { message });
   // res.status(statusCode).send(message);
+});
+
+
+let port = 8080;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
 // app.get("/testListing",async (req,res)=> {
