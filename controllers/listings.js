1368779e92b18f7bfc9780a8282ed51b4e5bc06b
Sumit Lone <sumitlone1912@gmail.com>
2025-10-11 23:40:09 +0530

    image url is replaced with upload image functionality

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,5 @@
 const Listing = require("../models/listing");
+const ExpressError = require("../utils/ExpressError");
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find();
@@ -27,8 +28,12 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res) => {
+  let url = req.file.path;
+  let filename = req.file.filename;
+
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
+  newListing.image = { url, filename };
   await newListing.save();
   req.flash("success", "New listing added successfully!");
   res.redirect("/listing");
@@ -53,6 +58,12 @@ module.exports.updateListing = async (req, res) => {
     runValidators: true,
     new: true,
   });
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { url, filename };
+    await listing.save();
+  }
   req.flash("success", "Listing updated successfully!");
   res.redirect(`/listing/${listing._id}`);
 };
--- routes/listing.js
+++ routes/listing.js
@@ -7,12 +7,17 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({storage});
+
 // index route
 router
   .route("/")
   .get(wrapAsync(listingController.index))
   .post(
     isLoggedIn,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.createListing)
   );
@@ -27,12 +32,11 @@ router
   .put(
     isLoggedIn,
     isOwner,
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.updateListing)
   )
   .delete(isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
-
-
 
 // edit route
 router.get(
