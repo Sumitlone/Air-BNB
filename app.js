d15db1c9bcf226486ebb84de32c9e46b44be7512
Sumit Lone <sumitlone1912@gmail.com>
2025-10-20 20:30:10 +0530

    connected with Mongo Atlas

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo'); 
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -18,6 +19,10 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
+
+// const MongoUrl = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
 // Database connection
 main()
   .then(() => {
@@ -29,7 +34,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -40,7 +45,20 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.engine("ejs", ejsMate);
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: "secretkey",
+  },
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("Session store error", e);
+});
+
 const sessionOptions = {
+  store,
   secret: "secretkey",
   resave: false,
   saveUninitialized: true,
